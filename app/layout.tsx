f245e67adda79e63b0b1e5897a13e5489062e091
fateh beddiaf <kentounmax@gmail.com>
2023-09-26 22:07:48 +0100

    add toaster provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ToastProvider } from "@/components/providers/toaster-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,8 +18,11 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en" suppressHydrationWarning={true}>
-        <body className={inter.className}>{children}</body>
+      <html lang="en">
+        <body className={inter.className} suppressHydrationWarning={true}>
+          <ToastProvider />
+          {children}
+        </body>
       </html>
     </ClerkProvider>
   );
